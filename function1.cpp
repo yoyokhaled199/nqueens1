c75e33a91e1ad4ad645dab0a91884e857cebc85f
omarriad2504 <omarriad@aucegypt.edu>
2022-06-07 17:10:39 +0200

    message

--- function1.cpp
+++ function1.cpp
@@ -1,28 +1,32 @@
-
-/* C++ program to solve N Queen Problem using
-   backtracking */
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 #define N 4
-/* ld is an array where its indices indicate row-col+N-1
- (N-1) is for shifting the difference to store negative
- indices */
-int ld[30] = { 0 };
-/* rd is an array where its indices indicate row+col
-   and used to check whether a queen can be placed on
-   right diagonal or not*/
-int rd[30] = { 0 };
-/*column array where its indices indicates column and
-  used to check whether a queen can be placed in that
-    row or not*/
-int cl[30] = { 0 };
-/* A utility function to print solution */
-void printSolution(int board[N][N])
-{
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
-            cout<<" "<< board[i][j]<<" ";
-        cout<<endl;
-    }
+void printBoard(int board[N][N]) {
+   for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++)
+         cout << board[i][j] << " ";
+         cout << endl;
+   }
 }
- 
+#include<iostream>
+using namespace std;
+#define N 4
+void printBoard(int board[N][N]) {
+   for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++)
+         cout << board[i][j] << " ";
+         cout << endl;
+   }
+}
+bool isValid(int board[N][N], int row, int col) {
+   for (int i = 0; i < col; i++) //check whether there is queen in the left or not
+      if (board[row][i])
+         return false;
+   for (int i=row, j=col; i>=0 && j>=0; i--, j--)
+      if (board[i][j]) //check whether there is queen in the left upper diagonal or not
+         return false;
+   for (int i=row, j=col; j>=0 && i<N; i++, j--)
+      if (board[i][j]) //check whether there is queen in the left lower diagonal or not
+         return false;
+   return true;
+}
---
+++ function1.h
@@ -0,0 +1,2 @@
+void printBoard(int board[N][N]);
+bool isValid(int board[N][N], int row, int col);
